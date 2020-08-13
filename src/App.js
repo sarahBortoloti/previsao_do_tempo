e2508d40e784693d575541e6c7d7ac4458e6f28c
sarahBortoloti <sarah.bortoloti.15@gmail.com>
2020-08-13 14:16:04 -0300

    ajustes e mapeamento dos icones

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+export default class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  apiBaseUrl = 'https://api.hgbrasil.com/weather?woid=449704&format=json-cors&locale=pt';
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      city: '',
+      forecast: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get(this.apiBaseUrl).then(
+      ({ data }) => {
+        this.setState({
+          city: data.results.city_name,
+          forecast: data.results.forecast,
+        })
+    });
+
+    // fetch(this.apiBaseUrl).then(
+    //   response => response.json()
+    // ).then(json => {
+    //   this.setState({
+    //     city: json.results.city_name
+    //   })
+    //   console.log(json);
+    // });
+  }
+
+  render() {
+  
+    return (
+      <div className="container">
+        <h2> { this.state.city }</h2>
+        <table className="striped centered">
+          <thead>
+            <tr>
+              <th> Data </th>
+              <th> Min. </th>
+              <th> Max. </th>
+              <th> Previsão </th>
+              <th>Img</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.forecast.map((day, index) => {
+                return(
+                  <tr key={index}>
+                    <td>{day.date}</td>
+                    <td>{day.min}</td>
+                    <td>{day.max}</td>
+                    <td>{day.description}</td>
+                    <td><img src={`/weather-icons/${day.condition}.svg`} alt={day.description}/></td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
-
-export default App;
